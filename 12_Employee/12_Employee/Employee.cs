e0dac3eb6d561597b2b07b76b518fdb326ceb202
gunnargh <gunnargeir89@gmail.com>
2016-10-10 23:46:47 +0200

    Add files via upload

---
+++ 12_Employee/12_Employee/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace _12_Employee
+{
+    internal class Employee
+    {
+        public string Name { get; internal set; }
+        public string Type { get; internal set; }
+        public int Id { get; internal set; }
+        
+        public Employee()
+        {
+        }
+
+        public Employee(string Name, string Type, int Id)
+        {
+            this.Name = Name;
+            this.Type = Type;
+            this.Id = Id;
+        }
+
+        
+    }
+}
---
+++ 12_Employee/12_Employee/EmployeeRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {
+        List<Employee> le = new List<Employee>();
+        int i = 0;
+        public int Id = 0;
+        internal void Clear()
+        {
+            le.Clear();
+            i = 0;
+        }
+
+        internal int CountEmployees()
+        {
+            foreach (Employee l in le)
+            {
+                i++;
+            }
+            return i;
+        }
+
+        internal int newint()
+        {
+            return Id++;
+        }
+
+        internal Employee CreateEmployee(string v1, string v2)
+        {
+            newint();
+            Employee e = new Employee();
+            e.Name = v1;
+            e.Type = v2;
+            e.Id = Id;
+            le.Add(e);
+            return e;
+        }
+
+        internal void SaveEmployee(Employee simon)
+        {
+            
+        }
+       
+
+        internal Employee LoadEmployee(int id)
+        {
+            Employee emp = new Employee();
+            foreach (Employee l in le)
+            {
+               if (l.Id  == id)
+                {
+                    emp = l;
+                }
+            }
+            return emp;
+        }
+
+        internal List<Employee> FindAllEmployees()
+        {
+            return le;
+        }
+    }
+}
